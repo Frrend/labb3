37d709951d221eeb4772dcee74764ae5e97335aa
Frrend <edualfd0514@gmail.com>
2024-12-10 09:20:46 +0100

    Searching after a car registration info.

--- main.c
+++ main.c
@@ -41,7 +41,7 @@ void add_vehicle(FILE *registrationFile) {
 }
 
 void vehicle_register(FILE *registrationFile) {
-    rewind(registrationFile); // Start reading from the beginning
+    rewind(registrationFile);
     struct Vehicle newVehicle;
     int i = 1;
     printf("___________________________\n");
@@ -52,6 +52,25 @@ void vehicle_register(FILE *registrationFile) {
             i, newVehicle.plateNumber, newVehicle.brand, newVehicle.vehicleType);
         i++;
     }
+}
+
+// TODO fix searching after unsigned car.
+void vehicle_search(FILE *registrationFile, char *_input, int *input) {
+    rewind(registrationFile);
+    struct Vehicle newVehicle;
+
+    fgets(_input, 3, stdin);
+    *input = atoi(_input) - 1;
+
+    for (int j = 0; sizeof(struct Vehicle) > j; j++) {
+        fseek(registrationFile, sizeof(struct Vehicle) * *input, SEEK_SET);
+        fread(&newVehicle, sizeof(struct Vehicle), 1, registrationFile);
+    }
+    printf(
+        "\n___________________________\nPlate number: %s\nVehicle brand: %s\nVehicle type: %s \nVehicle owner: %s\nOwner age: %s\n___________________________\n"
+        , newVehicle.plateNumber, newVehicle.brand, newVehicle.vehicleType, newVehicle.owner.name,
+        newVehicle.owner.age);
+    *input = 1;
 }
 
 int main(void) {
@@ -79,11 +98,9 @@ int main(void) {
                 case 3:
                     printf("Sort functionality not implemented yet.\n");
                     break;
-                case 4:
-                    printf("Show Vehicle functionality not implemented yet.\n");
+                case 4: vehicle_search(registrationFile, _input, &input);
                     break;
-                case 5:
-                    vehicle_register(registrationFile);
+                case 5: vehicle_register(registrationFile);
                     break;
                 default: printf("Invalid input, please try again.\n\n");
             }
