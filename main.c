354383f3b5e7d49e37b73be29ea66aad6e2b3f7f
Frrend <edualfd0514@gmail.com>
2024-12-12 14:52:15 +0100

    Fixed trying to search after car number -1 and 13. And fixed searching after unsigned car.

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 // TODO magic variables.
 struct Owner {
     char name[10];
@@ -15,7 +16,6 @@ struct Vehicle {
 };
 
 // TODO fix inputting numbers for age (int needed). The program should not crash.
-// TODO adding a Vehicle when the amount is full.
 void add_vehicle(FILE *registrationFile) {
     struct Vehicle newVehicle;
 
@@ -62,19 +62,21 @@ void vehicle_register(FILE *registrationFile) {
     }
 }
 
-// TODO trying to search after car number -1 and 13.
-// TODO fix searching after unsigned car.
-void vehicle_search(FILE *registrationFile, char *_input, int *input) {
+void vehicle_search(FILE *registrationFile) {
     rewind(registrationFile);
     struct Vehicle newVehicle;
+    int vehicleIndex;
+    char _vehicleIndex[3];
 
-    fgets(_input, 3, stdin);
-    *input = atoi(_input) - 1;
+    fgets(_vehicleIndex, 3, stdin);
+    vehicleIndex = atoi(_vehicleIndex) - 1;
 
-    for (int j = 0; sizeof(struct Vehicle) > j; j++) {
-        fseek(registrationFile, sizeof(struct Vehicle) * *input, SEEK_SET);
-        fread(&newVehicle, sizeof(struct Vehicle), 1, registrationFile);
+    fseek(registrationFile, sizeof(struct Vehicle) * vehicleIndex, SEEK_SET);
+    if (fread(&newVehicle, sizeof(struct Vehicle), 1, registrationFile) != 1) {
+        printf("No vehicle found at the given number.\n");
+        return;
     }
+
     printf(
         "\n___________________________\nPlate number: %s\nVehicle brand: %s\nVehicle type: %s \nVehicle owner: %s\nOwner age: %s\n___________________________\n"
         , newVehicle.plateNumber, newVehicle.brand, newVehicle.vehicleType, newVehicle.owner.name,
@@ -107,11 +109,12 @@ int main(void) {
                 case 3:
                     printf("Sort functionality not implemented yet.\n");
                     break;
-                case 4: vehicle_search(registrationFile, _input, &input);
+                case 4: vehicle_search(registrationFile);
                     break;
                 case 5: vehicle_register(registrationFile);
                     break;
-                case 0: fclose(registrationFile); return 0;
+                case 0: fclose(registrationFile);
+                    return 0;
                 default: printf("Invalid input, please try again.\n\n");
             }
         }
